7a89cb919c7a9b6a8bcd60d042e6336a3a833d33
Vikas <vikas@higgsbosonhealth.com>
2024-12-04 20:57:37 +0530

    Added Typography

---
+++ ios-mod-gacha-ref-02/Source/Typography.swift
@@ -0,0 +1,11 @@
+//
+//  Typography.swift
+//  ios-mod-gacha-ref-02
+//
+//  Created by Vikas Joshi on 04/12/24.
+//
+
+import Foundation
+
+/* A struct to store all typography used in the app. */
+struct Typography {}
