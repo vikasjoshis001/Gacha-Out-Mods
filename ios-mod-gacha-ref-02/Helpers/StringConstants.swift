2b2a0f1dcb758c6422697d8e203fb43eb3ee126d
Vikas <vikas@higgsbosonhealth.com>
2024-12-04 20:12:27 +0530

    Added StringConstants file in Helpers

---
+++ ios-mod-gacha-ref-02/Helpers/StringConstants.swift
@@ -0,0 +1,11 @@
+//
+//  StringConstants.swift
+//  ios-mod-gacha-ref-02
+//
+//  Created by Vikas Joshi on 04/12/24.
+//
+
+import Foundation
+
+/* A struct to store all string constants used in the app. */
+struct StringConstants {}
